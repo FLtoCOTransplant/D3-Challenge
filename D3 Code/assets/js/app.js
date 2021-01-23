ec923351587e3f51138c7950594e2b5568e1c84c
FLtoCOTransplant <67385063+FLtoCOTransplant@users.noreply.github.com>
2021-01-22 21:44:49 -0700

    working on outline

--- D3 Code/assets/js/app.js
+++ D3 Code/assets/js/app.js
@@ -1,11 +1,15 @@
 // Set the size
-let svgWidth = 960;
-let svgHeight = 620;
+var svgWidth = 960;
+var svgHeight = 500;
 
-// Set margins
-let margin = {
-    top: 20, 
-    right: 40, 
-    bottom: 200,
+// Set margins in svg
+var margin = {
+    top: 20,
+    right: 40,
+    bottom: 80,
     left: 100
   };
+
+// determine chart height and width
+var width = svgWidth - margin.left - margin.right;
+var height = svgHeight - margin.top - margin.bottom;
