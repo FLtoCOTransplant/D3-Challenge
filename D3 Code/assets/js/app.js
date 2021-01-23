2f3caf08e38879aabb5407802171ad35b88ccc92
FLtoCOTransplant <67385063+FLtoCOTransplant@users.noreply.github.com>
2021-01-22 21:38:39 -0700

    set size and margin

--- D3 Code/assets/js/app.js
+++ D3 Code/assets/js/app.js
@@ -1,1 +1,11 @@
-// @TODO: YOUR CODE HERE!
+// Set the size
+let svgWidth = 960;
+let svgHeight = 620;
+
+// Set margins
+let margin = {
+    top: 20, 
+    right: 40, 
+    bottom: 200,
+    left: 100
+  };
