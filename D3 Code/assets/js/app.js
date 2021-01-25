ba31d48ea82b370c7a213e172f01d5d119551084
FLtoCOTransplant <67385063+FLtoCOTransplant@users.noreply.github.com>
2021-01-24 21:52:02 -0700

    axis info

--- D3 Code/assets/js/app.js
+++ D3 Code/assets/js/app.js
@@ -25,3 +25,20 @@ var svg = d3
 // Append a SVG
 var chartGroup = svg.append("g")
 .attr("transform", `translate(${margin.left}, ${margin.top})`);
+
+// Define the axis
+var xAxis = "poverty";
+var yAxis = "healthcare";
+
+// function used for updating x-scale var upon click on axis label
+function xScale(data, xAxis) {
+  // create scales
+  var xLinearScale = d3.scaleLinear()
+    .domain([d3.min(data, d => d[xAxis]) * 0.9,
+      d3.max(data, d => d[xAxis]) * 1.1
+    ])
+    .range([0, width]);
+
+  return xLinearScale;
+
+}
